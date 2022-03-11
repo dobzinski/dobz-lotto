06e9a7c56e42f3f06b4e4943951e09a79a58efd1
Robson Dobzinski <robson.dobzinski@gmail.com>
2022-03-10 21:38:33 -0300

    Update generic.php
    adjusted code and more examples

--- scripts/generic.php
+++ scripts/generic.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 <?php
-
 /******************************************
 
 Robson Dobzinski
@@ -16,8 +15,19 @@ How to use:
 	$4 (OPTIONAL: List of fixed numbers: use comma or space to separate) \
 	$5 (OPTIONAL: Use to format "0" left chars, default are two)
 
-Example:
-  ~$ ./script.php 1-25 15 10 1,2,3,4,5
+Examples:
+
+  # No inform my numbers, random 1 to 60, select 6, and 10 loop
+  ~$ ./script.php 1-60 6 10 
+  
+  # Inform list of numbers, input after 20 loop
+  ~$ ./script.php 1-25 15 20 1,2,3,4,5
+  
+  # Another format to inform the numbers list
+  ~$ ./script.php 1-25 15 20 '2 4 6'
+  
+  # Start 90 to 110, will ignore 80 and 150, ignore 91 duplicate, and format 3 chars (084)
+  ~$ ./script.php 90-110 5 10 '84, 91, 100, 150, 91' 3
 
 ***/
 
@@ -58,7 +68,7 @@ if (count($list)>0) {
 		}
 		if (count($select)>0) {
 			$i = 0;
-			while($i < $total) {
+			while($i<$total) {
 				shuffle($select);
 				$n = 0;
 				foreach($select as $k) {
@@ -94,7 +104,7 @@ if (count($list)>0) {
 				$col = 0;
 				foreach($card as $v) {
 					echo str_pad($v, $format, '0', STR_PAD_LEFT);
-					if (count($card) > ($col+1)) {
+					if (count($card)>($col+1)) {
 						echo " - ";
 					}
 					$col++;
@@ -105,7 +115,7 @@ if (count($list)>0) {
 	}
 } else {
 	$i = 0;
-	while($i < $total) {
+	while($i<$total) {
 		shuffle($table);
 		$n = 0;
 		foreach($table as $t) {
@@ -124,7 +134,7 @@ if (count($list)>0) {
 			$col = 0;
 			foreach($k as $v) {
 				echo str_pad($v, $format, '0', STR_PAD_LEFT);
-				if (count($k) > ($col+1)) {
+				if (count($k)>($col+1)) {
 					echo " - ";
 				}
 				$col++;
@@ -134,4 +144,5 @@ if (count($list)>0) {
 	}
 }
 exit(0);
+
 ?>
